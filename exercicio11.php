cd0324fff43c017b44655f43ecda4c707c118184
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-07-10 08:37:16 -0300

    Feito o Exercicio 11

---
+++ exercicio11.php
@@ -0,0 +1,28 @@
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 11</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label for="texto_palindromo">Escreva uma palavra para ver se ela é um palíndromo: </label>
+        <input type="text" name="texto_palindromo" id="texto_palindromo" required>
+        <button type="submit" name="verificar_palindromo">Enviar</button>
+    </form>
+
+    <?php
+    
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            if(isset($_POST['verificar_palindromo'])){
+                $palavra = $_POST['texto_palindromo'];
+                $palavra_invertida = strrev($palavra);
+
+                echo "A palavra $palavra ".(($palavra_invertida == $palavra)? ' é um palíndromo':'não é um palíndromo');
+                
+            }
+        }
+
+    ?>
+</body>
+</html>
